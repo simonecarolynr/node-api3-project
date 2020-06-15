db6972cf76ca3c23612b73ea9eca20641d6c9814
Simone Roy <simonecarolynr@gmail.com>
2020-06-14 19:34:16 -0600

    MVP

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code away!
+
+require('dotenv').config();
+
+const server = require('./server');
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port} ^.^`)
+})
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter')
 const server = express();
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
+server.use(express.json());
+server.use(logger);
+server.use('/api/user', userRouter)
+server.use('/api/posts', postRouter)
 
-function logger(req, res, next) {}
+
+function logger(req, res, next) {
+  const {method, originalUrl} = req;
+  console.log( ` ${method} to ${originalUrl} at [${new Date().toISOString()}]`)
+  next();
+};
 
 module.exports = server;
